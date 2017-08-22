2b1e1f87fc322a079d77428efed412af8fe24cd1
AULA-7 <ivandiaz2422@gmail.com>
2017-08-22 12:36:54 -0500

    1

---
+++ app/src/main/java/com/example/aula_7/interactividad/Interactividad.java
@@ -0,0 +1,13 @@
+package com.example.aula_7.interactividad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Interactividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interactividad);
+    }
+}
