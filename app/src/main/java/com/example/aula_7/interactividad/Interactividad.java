b269217ed75c296de0a992ef7309e6592f52cf27
AULA-7 <ivandiaz2422@gmail.com>
2017-08-22 13:49:58 -0500

    3

--- app/src/main/java/com/example/aula_7/interactividad/Interactividad.java
+++ app/src/main/java/com/example/aula_7/interactividad/Interactividad.java
@@ -1,9 +1,11 @@
 package com.example.aula_7.interactividad;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Interactividad extends AppCompatActivity {
 
@@ -11,15 +13,26 @@ public class Interactividad extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interactividad);
+
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab_1);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goActivityDos(view);
+
+            }
+        });
+
+        Button btn_1=(Button) findViewById(R.id.btn_1);
+        registerForContextMenu(btn_1);
+
+
+
     }
 
     public void goActivityDos(View view){
         Intent intent = new Intent(this, Interactividad2.class);
         startActivity(intent);
-
-
-
-
     }
 
 }
--- app/src/main/java/com/example/aula_7/interactividad/Interactividad2.java
+++ app/src/main/java/com/example/aula_7/interactividad/Interactividad2.java
@@ -1,6 +1,7 @@
 package com.example.aula_7.interactividad;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,15 @@ public class Interactividad2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interactividad2);
+
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab_2);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goActivityUno(view);
+
+            }
+        });
     }
     public void goActivityUno(View view){
         Intent intent2 = new Intent(this, Interactividad.class);
